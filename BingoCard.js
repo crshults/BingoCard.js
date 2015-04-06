7c03e0f6e3cd16e29d2d5a5a08c35217b20cd642
crshults <chris.r.shults@gmail.com>
2015-04-06 14:01:13 -0500

    for..of was only happy in FireFox - switched to Array forEach

--- BingoCard.js
+++ BingoCard.js
@@ -1,35 +1,42 @@
 (function () {
 	function fix_aspect_ratios() {
-		for (x of document.getElementsByClassName("fixedaspectratio")) {
-			var aspect_ratio = JSON.parse(x.getAttribute("data-aspectratio"));
-			var newWidth = parseInt(
-				(x.parentNode.clientWidth/x.parentNode.clientHeight > (aspect_ratio.width/aspect_ratio.height)) ?
-					x.parentNode.clientHeight*(aspect_ratio.width/aspect_ratio.height) :
-					x.parentNode.clientWidth);
-			var newHeight = parseInt(
-				(x.parentNode.clientWidth/x.parentNode.clientHeight > (aspect_ratio.width/aspect_ratio.height)) ?
-					x.parentNode.clientHeight :
-					x.parentNode.clientWidth/(aspect_ratio.width/aspect_ratio.height));
-			x.style.width = newWidth + "px";
-			x.style.height = newHeight + "px";
-			x.style.left = parseInt(x.parentNode.clientWidth-newWidth)/2 + "px";
-			x.style.top = parseInt(x.parentNode.clientHeight-newHeight)/2 + "px";
-		}
+		Array.prototype.forEach.call(
+			document.getElementsByClassName("fixedaspectratio"),
+			function (x) {
+				var aspect_ratio = JSON.parse(x.getAttribute("data-aspectratio"));
+				var newWidth = parseInt(
+					(x.parentNode.clientWidth/x.parentNode.clientHeight > (aspect_ratio.width/aspect_ratio.height)) ?
+						x.parentNode.clientHeight*(aspect_ratio.width/aspect_ratio.height) :
+						x.parentNode.clientWidth);
+				var newHeight = parseInt(
+					(x.parentNode.clientWidth/x.parentNode.clientHeight > (aspect_ratio.width/aspect_ratio.height)) ?
+						x.parentNode.clientHeight :
+						x.parentNode.clientWidth/(aspect_ratio.width/aspect_ratio.height));
+				x.style.width = newWidth + "px";
+				x.style.height = newHeight + "px";
+				x.style.left = parseInt(x.parentNode.clientWidth-newWidth)/2 + "px";
+				x.style.top = parseInt(x.parentNode.clientHeight-newHeight)/2 + "px";
+			}
+		);
 	}
 
 	window.addEventListener('resize', fix_aspect_ratios);
 	window.addEventListener('load', fix_aspect_ratios);
 
 	function fix_font_sizes() {
-		for (x of document.getElementsByClassName("BingoCardText")) {
-			x.style.fontSize = (x.clientHeight * 0.8).toString() + "px";
-		}
+		Array.prototype.forEach.call(
+			document.getElementsByClassName("BingoCardText"),
+			function (x) {
+				x.style.fontSize = (x.clientHeight * 0.8).toString() + "px";
+			}
+		);
 	}
 
 	window.addEventListener('resize', fix_font_sizes);
 	window.addEventListener('load', fix_font_sizes);
 }());
 
+/*
 function BingoCard_onCommand(command) {
 	[cmd, data] = command;
 
@@ -57,6 +64,7 @@ function BingoCard_onCommand(command) {
 			break;
 	}
 }
+*/
 
 //BingoCard_onCommand(["fill", [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]);
 //BingoCard_onCommand(["daub", "I-2"]);
